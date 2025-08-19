8633ce3e6acc046012ca759efca2365afbaed477
Matheus <mrogeriopesarini@gmail.com>
2025-08-19 00:33:23 -0300

    First commit

---
+++ src/main/java/com/matheus/backend_spring_aws/BackendSpringAwsApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.backend_spring_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringAwsApplication.class, args);
+	}
+
+}
